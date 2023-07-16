32ed33aed63f40e1fadaf1e4b7d9632ae369e785
xingranzh <xingranzh@zju.edu.cn>
2023-07-16 08:09:38 +0000

    Deploying to gh_pages from @ xingranzh/kornia@ebd402c4a3b081896c02266d832d845937f14afc 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.6.7-dev',
-    LANGUAGE: 'None',
+    VERSION: '0.6.13-dev',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
